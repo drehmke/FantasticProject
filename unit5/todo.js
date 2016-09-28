f89bba83cd4e35c8527b54caceabf1875e64e12c
Donna Ryan <donna@awdd.net>
2016-09-27 20:55:51 -0700

    Added the ability to delete individual tasks

--- unit5/todo.js
+++ unit5/todo.js
@@ -1,4 +1,3 @@
-/* Can I add Date Due ? and Delete ? */
 var sessKey = "todo";
 var today = new Date();
 var todayDisplay = document.getElementById('todaysdate');
@@ -30,23 +29,31 @@ newForm.addEventListener('submit', function(e){
   createRow(newTodo);
 })
 
-// Toggle task completeness - any checkbox, on change, bubble up ---------------
-todoList.addEventListener('change', function(e){
-  e.preventDefault();
+
+//todoList.addEventListener('change', function(e){
+todoList.addEventListener('click', function(e){
+  //e.preventDefault();
   if( e.target.tagName == 'INPUT' ) {
+    // Toggle task completeness - any checkbox, on click, bubble up ------------
     var cid = e.target.id;
     // find the todo and change the completeness
     for( var i = 0; i < allTodos.length; i++ ) {
       if( allTodos[i].id == cid ) {
         if( allTodos[i].complete == true ) {
+          // oops, messed up! We weren't really done --------------
           allTodos[i].complete = false;
           allTodos[i].cdDate = false;
+          document.getElementById(cid).checked = false; // make sure the checkbox is unchecked
+          document.getElementById('cdID'+cid).innerHTML = '';
         } else {
+          // we are completing the task! Yay! ---------------------
           allTodos[i].complete = true;
           allTodos[i].cdDate = formatShortDate( today );
           // update the cell for Completed
           var cdCell = document.getElementById('cdID'+cid);
           cdCell.innerHTML = formatShortDate(today);
+          // update the checkbox
+          document.getElementById(cid).checked = true;
         }
       }
       //console.log(allTodos[i]);
@@ -54,6 +61,27 @@ todoList.addEventListener('change', function(e){
     // and we'll need to re-store allTodos
     var update = JSON.stringify(allTodos);
     localStorage.setItem(sessKey, update);
+  } else if (e.target.tagName == "BUTTON") {
+    // Delete this particular task, bubble up ----------------------------------
+    e.preventDefault();
+    console.log( e.target.value );
+    var remID = e.target.value;
+    var newTodos = [];
+    // remove it from allTodos
+    for( var i = 0; i < allTodos.length; i++ ) {
+      if( allTodos[i].id != remID ) {
+        newTodos.push(allTodos[i]);
+      }
+      // update allTodos
+      allTodos = newTodos;
+      // push the changes to localStorage
+      var update = JSON.stringify(allTodos);
+      localStorage.setItem(sessKey, allTodos);
+      // delete the table row
+      var remRow = document.getElementById("row"+remID);
+      remRow.remove();
+      // and we should be done
+    }
   }
 })
 
@@ -72,7 +100,7 @@ function createRow( todo )
   var tr;
   var tds;
   tr = document.createElement('tr');
-  tr.setAttribute('rowid', todo.id);
+  tr.setAttribute('id', "row"+todo.id);
   tds = createTd(todo.id, 'text'); // id
   tr.appendChild(tds);
   tds = createTd(todo.complete, 'checkbox', todo.id); // complete
@@ -84,6 +112,17 @@ function createRow( todo )
   tds = createTd(todo.cdDate, 'text'); // completed date
   tds.setAttribute('id', 'cdID'+todo.id);
   tr.appendChild(tds);
+
+  // Delete BUTTON
+  var btn = document.createElement('button');
+  btn.setAttribute('id', 'del'+todo.id);
+  btn.setAttribute('class', 'button delete');
+  btn.setAttribute('value', todo.id);
+  btn.innerHTML = "Delete Task";
+  var td = document.createElement('td'); // not running this through createTD because I don't need all that
+  td.appendChild(btn);
+  tr.appendChild(td);
+  // /end Delete BUTTON
   todoList.appendChild(tr);
 }
 
